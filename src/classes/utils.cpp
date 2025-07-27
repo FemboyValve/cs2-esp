9a58d43828c0f49f91e8580a208130e6e8962d0f
complainer3000 <complaining@lmbao.lol>
2025-07-28 01:24:02 +0200

    Update utils.cpp

--- src/classes/utils.cpp
+++ src/classes/utils.cpp
@@ -7,7 +7,9 @@ void Utils::update_console_title() {
     config::show_skeleton_esp ? title += " | Skeleton: [O]" : title += " | Skeleton: [X]";
     config::show_head_tracker ? title += " | Head Tracker: [O]" : title += " | Head Tracker: [X]";
     config::team_esp ? title += " | Team: [O]" : title += " | Team: [X]";
+#ifndef _UC
     config::automatic_update ? title += " | Auto Update: [O]" : title += " | Auto Update: [X]";
+#endif
     config::show_extra_flags ? title += " | Flags: [O]" : title += " | Flags: [X]";
 
 	SetConsoleTitle(title.c_str());
